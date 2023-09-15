29a18f9ffa450507900a24d61f95e86749ac8a90
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-15 11:23:41 +0900

    💄 スタイルの追加・スクロールのUXを追加

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -14,7 +14,7 @@ const Stories = () => {
   }))
 
   return (
-    <div>
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
       {storyUsers.map((user: StoryUser) => (
         <Story key={user.id} storyUser={user} />
       ))}
--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -10,8 +10,12 @@ type StoryProps = {
 const Story: React.FC<StoryProps> = ({ storyUser }) => {
   return (
     <div>
-      <img className="h-14" src={storyUser.img} alt={storyUser.username} />
-      <p>{storyUser.username}</p>
+      <img
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out"
+        src={storyUser.img}
+        alt={storyUser.username}
+      />
+      <p className="text-xs w-14 truncate">{storyUser.username}</p>
     </div>
   )
 }
