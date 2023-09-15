0f15879e077756f39208018a5afe41d91a6304ce
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-15 10:50:20 +0900

    💄 ヘッダーの作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,55 @@
+import { HomeIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import { PlusCircleIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+          <Image
+            src="http://www.jennexplores.com/wp-content/uploads/2015/09/Instagram_logo_black.png"
+            alt="Instagram"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+        <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+          <Image
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/800px-Instagram_logo_2016.svg.png"
+            alt="Instagram Icon"
+            layout="fill"
+            className="object-contain"
+          />
+        </div>
+
+        {/* Middle */}
+        <div className="relative mt-1">
+          <div className="absolute top-2 left-2">
+            <MagnifyingGlassIcon className="h-5 text-gray-500" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md"
+          />
+        </div>
+
+        {/* Right */}
+        <div className="flex space-x-4 items-center">
+          <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+          <img
+            src="https://static.skillshare.com/uploads/users/350301760/user-image-large.jpg?753816048"
+            alt="user-image"
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
