e2ff54a773a5386521c0faf4b128f5885500902b
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-20 07:04:53 +0900

    🚸 ログイン有無によってスタイリングを変更する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import Feed from '@/components/Feed'
+import { getServerSession } from 'next-auth'
 
-const Home = () => {
+import Feed from '@/components/Feed'
+import options from '@/libs/options'
+
+const Home = async () => {
+  const session = await getServerSession(options)
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+    <main
+      className={`grid ${
+        session
+          ? 'grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'
+          : 'grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto'
+      } `}
+    >
       <Feed />
     </main>
   )
