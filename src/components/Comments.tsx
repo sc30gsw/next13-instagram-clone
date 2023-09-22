1bb3dfb548c996de57adaba3b79f353a7db8ddcd
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-09-22 18:37:50 +0900

    💄 MUIのモーダルに変更

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,11 +1,11 @@
 'use client'
 
 import { EllipsisHorizontalIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Modal from '@mui/material/Modal'
 import { formatDistanceToNow } from 'date-fns'
 import { enUS } from 'date-fns/locale'
 import type { Session } from 'next-auth'
-import React, { useCallback, useEffect, useState } from 'react'
-import Modal from 'react-modal'
+import React from 'react'
 import { useRecoilState } from 'recoil'
 
 import { commentModalState } from '@/atom/commentModalAtom'
@@ -27,8 +27,6 @@ const Comments: React.FC<CommentsProps> = ({
   comments,
   commentCount,
 }) => {
-  useEffect(() => Modal.setAppElement('#commentModal'), [])
-
   const [open, setOpen] = useRecoilState(commentModalState)
 
   const closeModal = () => setOpen(false)
@@ -42,57 +40,58 @@ const Comments: React.FC<CommentsProps> = ({
         View all {commentCount} comments
       </p>
 
-      <Modal
-        className="max-w-lg w-[90%] p-6 absolute top-[50%] left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
-        isOpen={open}
-        onRequestClose={closeModal}
-      >
-        <div className="flex justify-end">
-          <XMarkIcon
-            onClick={closeModal}
-            className="cursor-pointer h-7 text-gray-400 hover:text-gray-600"
-          />
+      <Modal open={open} onClose={closeModal}>
+        <div className="max-w-lg w-[90%] p-6 absolute top-[50%] left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md">
+          <div className="flex justify-end">
+            <XMarkIcon
+              onClick={closeModal}
+              className="cursor-pointer h-7 text-gray-400 hover:text-gray-600"
+            />
+          </div>
+          <div className="flex items-center border-b py-4 mb-2">
+            {/*eslint-disable-next-line @next/next/no-img-element*/}
+            <img
+              className="h-7 rounded-full object-cover mr-3"
+              src={post.user.image ? post.user.image : '/images/noAvatar.png'}
+              alt={post.user.name}
+            />
+            <p className="font-bold flex-1">{post.user.name}</p>
+            <EllipsisHorizontalIcon className="h-7" />
+          </div>
+          <div className="max-h-24 overflow-y-scroll scrollbar-none">
+            {comments.map((comment: Comment) => (
+              <div
+                key={comment.id}
+                className="flex items-center space-x-2 mb-2"
+              >
+                {/*eslint-disable-next-line @next/next/no-img-element*/}
+                <img
+                  className="h-7 rounded-full object-cover"
+                  src={
+                    comment.user.image
+                      ? comment.user.image
+                      : '/images/noAvatar.png'
+                  }
+                  alt={comment.user.name}
+                />
+                <p className="font-semibold">{comment.user.name}</p>
+                <p className="flex-1 truncate">{comment.comment}</p>
+                <p>
+                  {formatDistanceToNow(new Date(comment.updatedAt), {
+                    addSuffix: true,
+                    locale: enUS,
+                  })}
+                </p>
+              </div>
+            ))}
+          </div>
+          {session && (
+            <>
+              <PostButton post={post} />
+              <PostInputBox postId={post.id} isModal={true} />
+            </>
+          )}
         </div>
-        <div className="flex items-center border-b py-4 mb-2">
-          {/*eslint-disable-next-line @next/next/no-img-element*/}
-          <img
-            className="h-7 rounded-full object-cover mr-3"
-            src={post.user.image ? post.user.image : '/images/noAvatar.png'}
-            alt={post.user.name}
-          />
-          <p className="font-bold flex-1">{post.user.name}</p>
-          <EllipsisHorizontalIcon className="h-7" />
-        </div>
-        <div className="max-h-24 overflow-y-scroll scrollbar-none">
-          {comments.map((comment: Comment) => (
-            <div key={comment.id} className="flex items-center space-x-2 mb-2">
-              {/*eslint-disable-next-line @next/next/no-img-element*/}
-              <img
-                className="h-7 rounded-full object-cover"
-                src={
-                  comment.user.image
-                    ? comment.user.image
-                    : '/images/noAvatar.png'
-                }
-                alt={comment.user.name}
-              />
-              <p className="font-semibold">{comment.user.name}</p>
-              <p className="flex-1 truncate">{comment.comment}</p>
-              <p>
-                {formatDistanceToNow(new Date(comment.updatedAt), {
-                  addSuffix: true,
-                  locale: enUS,
-                })}
-              </p>
-            </div>
-          ))}
-        </div>
-        {session && (
-          <>
-            <PostButton post={post} />
-            <PostInputBox postId={post.id} isModal={true} />
-          </>
-        )}
       </Modal>
     </div>
   )
--- src/components/UploadModal.tsx
+++ src/components/UploadModal.tsx
@@ -2,11 +2,11 @@
 
 import { CameraIcon, XCircleIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { zodResolver } from '@hookform/resolvers/zod'
+import Modal from '@mui/material/Modal'
 import { redirect, useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import Modal from 'react-modal'
 import { useRecoilState } from 'recoil'
 
 import { modalState } from '@/atom/modalAtom'
@@ -22,8 +22,6 @@ const UploadModal = () => {
   >(null)
 
   const router = useRouter()
-
-  useEffect(() => Modal.setAppElement('#modal'), [])
 
   const {
     register,
@@ -76,73 +74,71 @@ const UploadModal = () => {
   return (
     <div id="modal">
       {open && (
-        <Modal
-          className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md"
-          isOpen={open}
-          onRequestClose={closeModal}
-        >
-          <div className="flex justify-end">
-            <XMarkIcon
-              onClick={closeModal}
-              className="cursor-pointer h-7 text-gray-400 hover:text-gray-600"
-            />
-          </div>
-          <form
-            className="flex flex-col justify-center items-center h-full"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            {selectedFile ? (
-              <div className="w-full h-full relative">
-                <div className="absolute top-2 right-2">
-                  <XCircleIcon
-                    onClick={() => {
-                      setSelectedFile(null)
-                      reset()
-                    }}
-                    className="cursor-pointer h-10 text-gray-800 hover:brightness-200"
+        <Modal open={open} onClose={closeModal}>
+          <div className="max-w-lg w-[90%] p-6 absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md">
+            <div className="flex justify-end">
+              <XMarkIcon
+                onClick={closeModal}
+                className="cursor-pointer h-7 text-gray-400 hover:text-gray-600"
+              />
+            </div>
+            <form
+              className="flex flex-col justify-center items-center h-full"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {selectedFile ? (
+                <div className="w-full h-full relative">
+                  <div className="absolute top-2 right-2">
+                    <XCircleIcon
+                      onClick={() => {
+                        setSelectedFile(null)
+                        reset()
+                      }}
+                      className="cursor-pointer h-10 text-gray-800 hover:brightness-200"
+                    />
+                  </div>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    src={selectedFile as string}
+                    alt=""
+                    className="w-full max-h-[250px] object-cover"
                   />
                 </div>
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src={selectedFile as string}
-                  alt=""
-                  className="w-full max-h-[250px] object-cover"
+              ) : (
+                <CameraIcon
+                  onClick={() => filePickerRef.current?.click()}
+                  className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500"
                 />
-              </div>
-            ) : (
-              <CameraIcon
-                onClick={() => filePickerRef.current?.click()}
-                className="cursor-pointer h-14 bg-red-200 p-2 rounded-full border-2 text-red-500"
-              />
-            )}
+              )}
 
-            <input
-              {...register('file')}
-              type="file"
-              hidden
-              ref={filePickerRef}
-              onChange={addImageToPost}
-            />
-            {errors.file && (
-              <span className="text-red-500">{errors.file.message}</span>
-            )}
-            <input
-              {...register('caption')}
-              type="text"
-              maxLength={150}
-              placeholder="Please enter your caption..."
-              className="m-4 border-none text-center w-full focus:ring-0"
-            />
-            {errors.caption && (
-              <span className="text-red-500">{errors.caption.message}</span>
-            )}
-            <button
-              type="submit"
-              className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125"
-            >
-              Upload Post
-            </button>
-          </form>
+              <input
+                {...register('file')}
+                type="file"
+                hidden
+                ref={filePickerRef}
+                onChange={addImageToPost}
+              />
+              {errors.file && (
+                <span className="text-red-500">{errors.file.message}</span>
+              )}
+              <input
+                {...register('caption')}
+                type="text"
+                maxLength={150}
+                placeholder="Please enter your caption..."
+                className="m-4 border-none text-center w-full focus:ring-0"
+              />
+              {errors.caption && (
+                <span className="text-red-500">{errors.caption.message}</span>
+              )}
+              <button
+                type="submit"
+                className="w-full bg-red-600 text-white p-2 shadow-md hover:brightness-125"
+              >
+                Upload Post
+              </button>
+            </form>
+          </div>
         </Modal>
       )}
     </div>
